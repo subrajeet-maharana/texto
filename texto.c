5e94411669befc6a8ae2d06524be5dcc859056fa
subrajeet-maharana <subrajeet.info@gmail.com>
2022-12-08 19:07:54 +0530

    changed the texto.c file

--- texto.c
+++ texto.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+#include<termios.h>
+void enableRawMode(){
+    struct termios raw;
+    tcgetattr(STDIN_FILEN0(),&raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO(),TSCAFLUSH,&raw);
+}
 int main(){
+    enableRawMode();
     char c;
     while(read(STDIN_FILEN0(),&c,1)==1 && c!='q');
     return 0;
